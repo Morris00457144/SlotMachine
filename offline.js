84c976aceda38cc8c95bdc86f185700281141071
Morris00457144 <Morris00457144@users.noreply.github.com>
2019-07-23 23:59:22 +0800

    Finish SlotMachine

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1563894179,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/slotmachine-sheet0.png",
+		"images/pic1-sheet0.png",
+		"images/pic1-sheet1.png",
+		"images/point-sheet0.png",
+		"images/point2-sheet0.png",
+		"media/button-25.m4a",
+		"media/button-25.ogg",
+		"media/button-29.m4a",
+		"media/button-29.ogg",
+		"media/button-37.m4a",
+		"media/button-37.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"game config.json"
+	]
+}
